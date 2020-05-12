dea018748779bb4818c58ac680939721a7b225ef
Gergely Paless <gergely.paless@gmail.com>
2020-05-12 17:03:03 +0200

    \ changed to / in path

--- src/main/java/com/csakcintanyer/bme/projlab/View.java
+++ src/main/java/com/csakcintanyer/bme/projlab/View.java
@@ -21,30 +21,30 @@ public class View
 	private MainWindow mainWindow;
 	private Graphics g;
 	
-	public ImageIcon backgroundIcon = new ImageIcon("Assets\\Background.png");
-	public ImageIcon layoutIcon = new ImageIcon("Assets\\Layout.png");
+	public ImageIcon backgroundIcon = new ImageIcon("Assets/Background.png");
+	public ImageIcon layoutIcon = new ImageIcon("Assets/Layout.png");
 	
-	public ImageIcon snowyIceBlockIcon = new ImageIcon("Assets\\IceBlocks\\SnowyIceBlock.png");
-	public ImageIcon icyIceBlockIcon = new ImageIcon("Assets\\IceBlocks\\IcyIceBlock.png");
-	public ImageIcon emptyIceBlockIcon = new ImageIcon("Assets\\IceBlocks\\EmptyIceBlock.png");
+	public ImageIcon snowyIceBlockIcon = new ImageIcon("Assets/IceBlocks/SnowyIceBlock.png");
+	public ImageIcon icyIceBlockIcon = new ImageIcon("Assets/IceBlocks/IcyIceBlock.png");
+	public ImageIcon emptyIceBlockIcon = new ImageIcon("Assets/IceBlocks/EmptyIceBlock.png");
 	
-	public ImageIcon bulletIcon = new ImageIcon("Assets\\Items\\bullet.png");
-	public ImageIcon flareIcon = new ImageIcon("Assets\\Items\\flare.png");
-	public ImageIcon gunIcon = new ImageIcon("Assets\\Items\\gun.png");
-	public ImageIcon tentIcon = new ImageIcon("Assets\\Items\\tent.png");
-	public ImageIcon fragileShovelIcon = new ImageIcon("Assets\\Items\\fshovel.png");
-	public ImageIcon shovelIcon = new ImageIcon("Assets\\Items\\shovel.png");
-	public ImageIcon ropeIcon = new ImageIcon("Assets\\Items\\rope.png");
-	public ImageIcon suitIcon = new ImageIcon("Assets\\Items\\suit.png");
-	public ImageIcon foodIcon = new ImageIcon("Assets\\Background.png");
+	public ImageIcon bulletIcon = new ImageIcon("Assets/Items/bullet.png");
+	public ImageIcon flareIcon = new ImageIcon("Assets/Items/flare.png");
+	public ImageIcon gunIcon = new ImageIcon("Assets/Items/gun.png");
+	public ImageIcon tentIcon = new ImageIcon("Assets/Items/tent.png");
+	public ImageIcon fragileShovelIcon = new ImageIcon("Assets/Items/fshovel.png");
+	public ImageIcon shovelIcon = new ImageIcon("Assets/Items/shovel.png");
+	public ImageIcon ropeIcon = new ImageIcon("Assets/Items/rope.png");
+	public ImageIcon suitIcon = new ImageIcon("Assets/Items/suit.png");
+	public ImageIcon foodIcon = new ImageIcon("Assets/Background.png");
 	
-	public ImageIcon eskimoIcon = new ImageIcon("Assets\\Characters\\Eskimo1.png");
-	public ImageIcon explorerIcon = new ImageIcon("Assets\\Characters\\Explorer1.png");
+	public ImageIcon eskimoIcon = new ImageIcon("Assets/Characters/Eskimo1.png");
+	public ImageIcon explorerIcon = new ImageIcon("Assets/Characters/Explorer1.png");
 	
-	public ImageIcon bearIcon = new ImageIcon("Assets\\Bear.png");
+	public ImageIcon bearIcon = new ImageIcon("Assets/Bear.png");
 	
-	public ImageIcon iglooIcon = new ImageIcon("Assets\\Igloo.png");
-	public ImageIcon tentOnBlockIcon = new ImageIcon("Assets\\TentOnBlock.png");
+	public ImageIcon iglooIcon = new ImageIcon("Assets/Igloo.png");
+	public ImageIcon tentOnBlockIcon = new ImageIcon("Assets/TentOnBlock.png");
 	
 	public void init(int x, int y)
 	{
